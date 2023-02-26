3b9b9f68e305a13269811871177d3bb891aca40a
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-02-26 12:10:30 -0800

    prettier

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-function newImage (url, left, bottom) {
-let pic = document.createElement('img');
-pic.src = url;
-pic.style.position = 'fixed';
-pic.style.left = (left) + 'px';
-pic.style.bottom = (bottom) + 'px';
-document.body.append(pic);
-return pic;
+function newImage(url, left, bottom) {
+  let pic = document.createElement("img");
+  pic.src = url;
+  pic.style.position = "fixed";
+  pic.style.left = left + "px";
+  pic.style.bottom = bottom + "px";
+  document.body.append(pic);
+  return pic;
 }
 
-function newItem (url, left, bottom) {
-let pic = newImage (url, left, bottom);
+function newItem(url, left, bottom) {
+  let pic = newImage(url, left, bottom);
 
-pic.addEventListener('dblclick', function(){
+  pic.addEventListener("dblclick", function () {
     pic.remove();
-})
+  });
 }
 
-newImage('assets/green-character.gif', 100, 100);
-newImage('assets/pine-tree.png', 450, 200);
-newImage('assets/tree.png', 200, 300);
-newImage('assets/pillar.png', 350, 100);
-newImage('assets/crate.png', 150, 200);
-newImage('assets/well.png', 500, 425);
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
 
-newItem('assets/sword.png', 500, 405);
-newItem('assets/sheild.png', 165, 185);
-newItem('assets/staff.png', 600, 100);
+newItem("assets/sword.png", 500, 405);
+newItem("assets/sheild.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
