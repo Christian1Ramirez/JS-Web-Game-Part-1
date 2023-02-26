d8970ac663ef3a606aaea6a53f2d292bf72321e8
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-02-26 14:04:43 -0800

    background

--- index.js
+++ index.js
@@ -26,3 +26,18 @@ newImage("assets/well.png", 500, 425);
 newItem("assets/sword.png", 500, 405);
 newItem("assets/sheild.png", 165, 185);
 newItem("assets/staff.png", 600, 100);
+
+// background
+
+function bgImages(url, width, height, margin) {
+  let bg = document.createElement("div");
+  bg.style.backgroundImage = `url(${url})`;
+  bg.style.width = width + "%";
+  bg.style.height = height + "rem";
+  bg.style.margin = margin + "px";
+  document.body.appendChild(bg);
+  return bg;
+}
+
+bgImages("assets/sky.png", 100, 30, 0);
+bgImages("assets/grass.png", 100, 63, 0);
